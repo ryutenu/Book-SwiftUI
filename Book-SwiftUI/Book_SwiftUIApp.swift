04f2719a194098cc9102a51269b68a41d34f34b6
Alan Liu <ryutenu.tokyo@gmail.com>
2023-04-07 06:15:15 +0900

    [Add] Add files

---
+++ Book-SwiftUI/Book_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_SwiftUIApp.swift
+//  Book-SwiftUI
+//
+//  Created by Alan Liu on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct Book_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
